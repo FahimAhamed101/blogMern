0b86b1c08fba27eab3155e3beb6e852d1a101c3e
fahim <fahimahamedweb@gmail.com>
2024-01-19 03:50:21 +0600

    first commit

---
+++ api/index.js
@@ -0,0 +1,25 @@
+import path from 'path';
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const __dirname = path.resolve();
+
+const app = express();
+
+app.use(express.json());
+
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
